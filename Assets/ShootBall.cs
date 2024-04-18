29aff9c743bfb53b0cd0b0ba3a266606c9a21131
Volsca <jaco2268@yahoo.fr>
2024-04-18 16:00:27 +0200

    First Push
    Current funcionalities :
    - MR w Passthrough in quest 3
    - A cube (Incredible)
    - Projectiles shot from right controller
    - Occlusion of VR objects using Metas Depth API
    
    Envisioned functionalities :
    - Adding an avatar (Or portions of one)
    - A switch between full VR and MR
    - More interaction with virtual objects
    - VR UI used to maybe spawn objects in the world to interact with
    - Real world objects tracked in VR that can interact with VR objects
    - A way to switch those between VR and real maybe
    - envision a real world scenario that could be studied in this MR world
    - Anything else that comes to mind later down the road

---
+++ Assets/ShootBall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour
+{
+    public GameObject Projectile;
+    public float shootSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Projectile == null)
+        {
+            Debug.Log("ERR : Projectile undef");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            GameObject SpawnedObject = Instantiate(Projectile, transform.position, Quaternion.identity);
+            SpawnedObject.GetComponent<Rigidbody>().velocity = transform.forward * shootSpeed;
+        }
+    }
+}
