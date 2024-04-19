8d397a6fa1344f64528b5ecdca590186d447cbe0
Volsca <jaco2268@yahoo.fr>
2024-04-19 14:55:48 +0200

    Intermediary push
    Progress :
    - Full switch from OVR/OpenXR to full OVR thanks to better support for Quest 3 functionalities, and avoidance of bugs due to mixing to different VR implementations. (Passthrough and all meshes/Occlusion working corretly)
    - UI non aligned and unfonctional (OVR incompatible with OpenXR by default, though could just change the UI prefab by one that is)
    - Balls should be grabbable, to be tested.

--- Assets/UI_Controller.cs
+++ Assets/UI_Controller.cs
@@ -13,7 +13,13 @@ public class UI_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        WristUI = Instantiate(UI_Prefab, transform.position, Quaternion.identity);
+        // Starting position of the UI, moving it to be at the side of the wrist
+        Vector3 startPosition = transform.position;
+        //startPosition.x += 0.3f;
+        startPosition.z += -0.3f;
+
+        WristUI = Instantiate(UI_Prefab, startPosition, Quaternion.identity);
+        WristUI.transform.parent = this.transform;
         WristUI.SetActive(false);
     }
 
@@ -21,10 +27,11 @@ public class UI_Controller : MonoBehaviour
     void Update()
     {
         // If controller Y axis alligned with camera.forward axis then show UI
-        Vector3 ControllerY = this.transform.up.normalized;
+        Vector3 ControllerY = this.transform.right.normalized;
         Vector3 HMDForward = MainCamera.transform.forward.normalized;
+        WristUI.transform.rotation = Quaternion.LookRotation(HMDForward, Vector3.up);
 
-        if(Vector3.Dot(ControllerY, HMDForward) <= 1 - margin)
+        if(Vector3.Dot(HMDForward, ControllerY) >= 1 - margin)
         {
             WristUI.SetActive(true);
         }
