54e58c47a904ba2f0335070b34377f3e30c72c6b
Volsca <jaco2268@yahoo.fr>
2024-06-12 16:45:11 +0200

    Play button suddenly working
    So imma push to be able to go back

--- Assets/ExperienceController.cs
+++ Assets/ExperienceController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class ExperienceController : MonoBehaviour
 {
     public GlobalVariables GV;
+    public GameObject can;
 
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.One))
+        if (OVRInput.GetDown(OVRInput.Button.One))
         {
+            //Debug.Log("Button pressed");
             GV._DebugWindow.writeDebugMessage("Button pressed", 0, "");
+            onButtonPress();
         }
     }
 
     void onButtonPress()
     {
-        
+        can.transform.localPosition = GV.r_Controller.transform.localPosition;
     }
 }
